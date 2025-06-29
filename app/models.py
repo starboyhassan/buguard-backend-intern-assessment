36c199dd4807e7d1719902f6d8e2db190073dbc7
StarboyHassan <StarboyHassan11@gmail.com>
2025-06-29 16:25:15 +0300

    create models file for DB tables

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+#enumerated types for fixed choices (TaskStatus, TaskPriority)
+from enum import Enum
+
+from datetime import datetime, timezone
+
+
+class TaskStatus(str, Enum):
+    pending = "pending"
+    in_progress = "in_progress"
+    completed = "completed"
+    cancelled = "cancelled"
+
+class TaskPriority(str, Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+    urgent = "urgent"
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str = Field(max_length=200)
+    description: Optional[str] = Field(default=None, max_length=1000)
+    status: TaskStatus = Field(default=TaskStatus.pending)  # task status = pending by DEFAULT
+    priority: TaskPriority = Field(default=TaskPriority.medium) # task priority = medium by DEFAULT
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))  # this method 'datetime.now(timezone.utc)' will work with all python versions (even 3.11+)
+    updated_at: Optional[datetime] = None
+    due_date: Optional[datetime] = None
+    assigned_to: Optional[str] = Field(default=None, max_length=100)
